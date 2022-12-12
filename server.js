7c2e75a29a8b4aa5f48f529338e42bc30034ebb3
Vitalii Braun <vitossvx@gmail.com>
2022-12-12 16:38:55 +0300

    Add express server

---
+++ server.js
@@ -0,0 +1,12 @@
+
+const express = require("express");
+const app = express();
+
+app.use('/', express.static('dist'))
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+   console.log(`Server is running on PORT: ${PORT}`);
+});
+
